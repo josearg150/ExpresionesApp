3f177c55fe8c84918e09719a1b35e2805c504c7c
Jose angel <73683594+josearg150@users.noreply.github.com>
2021-09-01 07:34:05 -0500

    commit inicial

---
+++ Expresiones/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Expresiones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEntrada = new System.Windows.Forms.Label();
+            this.tbExpresion = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblEntrada
+            // 
+            this.lblEntrada.AutoSize = true;
+            this.lblEntrada.Location = new System.Drawing.Point(22, 22);
+            this.lblEntrada.Name = "lblEntrada";
+            this.lblEntrada.Size = new System.Drawing.Size(111, 13);
+            this.lblEntrada.TabIndex = 0;
+            this.lblEntrada.Text = "Ingresa una expresion";
+            this.lblEntrada.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tbExpresion
+            // 
+            this.tbExpresion.Location = new System.Drawing.Point(25, 55);
+            this.tbExpresion.Name = "tbExpresion";
+            this.tbExpresion.Size = new System.Drawing.Size(100, 20);
+            this.tbExpresion.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbExpresion);
+            this.Controls.Add(this.lblEntrada);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEntrada;
+        private System.Windows.Forms.TextBox tbExpresion;
+    }
+}
+
---
+++ Expresiones/Nodo.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expresiones
+{
+    /// <summary>
+    /// Clase nodo para la clase Arbol binario 
+    /// Contiene los atributos de un Nodo 
+    /// </summary>
+    /// 
+    /// <Para>
+    /// Con esta clase se pueden crear los nodos que seran usados por el arbol binario 
+    /// </Para>
+    /// 
+    /// <Supuestos>
+    /// </Supuestos>
+    /// 
+    /// <Autor>
+    /// Jose angel rocha garcia 
+    /// Jose luis carreon reyes
+    /// </Autor>
+    /// 
+    /// <FechaCreacion >
+    /// 30/08/2021
+    /// </FechaCreacion>
+    class Nodo
+    {
+        //***************************************
+        //Variables locales                     
+        //***************************************
+        #region Variables Locales 
+        private char caracter;
+        private Nodo izquierdo;
+        private Nodo derecho;
+        #endregion
+
+        //***************************************
+        //Constructores
+        //***************************************
+        #region Constructores 
+        //Constructor sin parametros 
+        public Nodo()
+        {
+            caracter = ' ';
+            izquierdo = null;
+            derecho = null;
+        }
+
+        //Constructor con parametros 
+        public Nodo(char _caracter)
+        {
+            caracter = _caracter;
+            izquierdo = null;
+            derecho = null;
+        }
+        public Nodo(char _caracter, Nodo _izquierdo, Nodo _derecho)
+        {
+            caracter = _caracter;
+            izquierdo = _izquierdo;
+            derecho = _derecho;
+        }
+        #endregion
+        //***************************************
+        //Metodos 
+        //***************************************
+        #region Metodos
+
+        public Nodo obtenerDerecho()
+        {
+            return derecho;
+        }
+
+        public Nodo obtenerIzquierdo()
+        {
+            return izquierdo;
+        }
+
+        public void setCaracter(char _caracter)
+        {
+            caracter = _caracter;
+        }
+        public Nodo insertar(Nodo a, Nodo b)
+        {
+            //Comparamos si su lado izquierdo para saber si esta vacio 
+            if (b.caracter.CompareTo(a.caracter) < 0)
+            {
+                if (a.izquierdo == null)
+                {
+                    a.izquierdo = b;
+                    return a;
+                }//Seguimos insertando hasta que tenga un lado izquierdo disponible 
+                else
+                {
+                    a.izquierdo = insertar(a.izquierdo, b);
+                    return a;
+                }
+            }//Hacemos el mismo procedimiento en el lado derecho 
+            else if (b.caracter.CompareTo(a.caracter) > 0)
+            {
+                if (a.derecho == null)
+                {
+                    a.derecho = b;
+                    return a;
+                }
+                else
+                {
+                    a.derecho = insertar(a.derecho, b);
+                    return a;
+                }
+            }//si ambos lados estan llenos regresamos null 
+            else
+            {
+                return null;
+            }
+        }
+        #endregion
+    }
+}
