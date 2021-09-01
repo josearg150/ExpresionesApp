f6beba59cbe26e50302ed9fbf5d879dce2a9e421
Jose angel <73683594+josearg150@users.noreply.github.com>
2021-09-01 10:50:10 -0500

    clase arbol agregada

---
+++ Expresiones/Arbol.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expresiones
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// 
+    /// <Para>
+    ///
+    /// </Para>
+    /// 
+    /// <Supuestos>
+    /// </Supuestos>
+    /// 
+    /// <Autor>
+    /// Jose angel rocha garcia 
+    /// Jose luis carreon reyes
+    /// </Autor>
+    /// 
+    /// <FechaCreacion >
+    ///
+    /// </FechaCreacion>
+    class Arbol
+    {
+    }
+}
